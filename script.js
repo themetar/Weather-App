373d7b844aa07869dff8f5aa53aeb5c81d667980
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-08-05 20:09:03 +0200

    Interact with openweathermap.org API

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const apiKey = "b2f923af492ef49f597b7a7055e0035f";
+
+async function getCurrentWeather(location = "Bitola") {
+  try {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      return Promise.reject("no-data");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  } 
+}
+
+getCurrentWeather("London").then(data => console.log(data));
